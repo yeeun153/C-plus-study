fdd8de3b6164d48811ac2dcbe7d5afab8e3ecc88
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-04 00:14:01 +0900

    Add files via upload

---
+++ printStr.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i <= 10; i++)
+		sum += i;
+
+	cout << "1에서 10까지 더한 결과는 " << sum << "입니다\n";
+	return 0;
+}
