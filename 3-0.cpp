9147ea30b6c7438f531fa056e3698f1d110f99af
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 23:54:54 +0900

    Add files via upload

---
+++ 3-0.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+//클래스 작성하는 위치
+
+//클래스 선언부
+class Circle {
+public:
+	int radius;
+	double getArea();
+};
+
+//클래스 구현부
+double Circle::getArea() {
+	return 3.14*radius*radius;
+}
+
+int main() {
+	Circle donut; //객체
+	donut.radius = 1;
+	cout << "donut 면적은 " << donut.getArea() << endl;
+
+	Circle pizza; //객체
+	pizza.radius = 30;
+	cout << "pizza 면적은 " << pizza.getArea() << endl;
+}
