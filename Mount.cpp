9c20dafb86128c777473c6204c4e75cb8ddddb4c
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-04 00:22:49 +0900

    Add files via upload

---
+++ Mount.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Mount {
+	int height;
+	string name;
+};
+
+int main() {
+	string names[4];
+	int i = 0;
+
+	for (int i = 0; i < 4; i++) {
+		cout << "입력>> ";
+		getline(cin, names[i], '\n');
+	}
+	cout << names[0] << endl;
+	cout << names[1] << endl;
+	cout << names[2] << endl;
+	cout << names[3] << endl;
+
+	int size = names->length();
+
+	cout << "===== The longest mountain name ====" << endl;
+	cout << "Annapurna(8091m)" << endl;
+}
+
