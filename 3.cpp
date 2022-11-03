89865b8175302dd16b134ee6f8e518f11b3d855c
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-04 00:09:47 +0900

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	string yes;
+
+	cout << "종료하고 싶으면 yes를 입력하세요 >>";
+
+	char word[100];
+	cin.getline(word, 100, '\n');
+
+	while (true) {
+		cout << "종료하고 싶으면 yes를 입력하세요 >>";
+		cin >> word;
+		if (strcmp(word, "yes") == 0) {
+			cout << "종료합니다..." << endl;
+			break;
+		}
+	}
+}
