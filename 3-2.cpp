35252b4cf1684d04432da3bb44343f3a56daf8c4
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 23:55:23 +0900

    Add files via upload

---
+++ 3-2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+//Rectangle 클래스 선언부
+class Rectangle {
+public:
+	int width;
+	int height;
+
+	Rectangle();
+	Rectangle(int width,int height);
+	int getArea();
+
+};
+
+int Rectangle::getArea() {
+	return width * height;
+}
+
+Rectangle::Rectangle() : Rectangle(1, 1) {
+	//width = height = 1;
+}
+
+Rectangle::Rectangle(int width, int height) {
+	this->width = width;
+	this->height = height;
+}
+
+int main() {
+	Rectangle rect; //(1)
+	rect.width = 3;
+	rect.height = 5;
+	cout << "사각형의 면적은 " << rect.getArea() << endl;
+
+
+	Rectangle rect2(2, 3); //(2)
+	cout << "사각형의 면적은 " << rect2.getArea() << endl;
+}
